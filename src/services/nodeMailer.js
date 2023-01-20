485c721dfb03315a529852472d5ff272fd75d677
lautarosantillan <santillanlautaro03@gmail.com>
2023-01-20 20:48:18 -0300

    Credentials update and running NodeMailer & Twilio -v15

--- src/services/nodeMailer.js
+++ src/services/nodeMailer.js
@@ -1,56 +1,59 @@
-import { createTransport } from 'nodemailer';
-import { logger } from "../utils/logger.js";
-import dotenv from 'dotenv';
-dotenv.config();
+import { createTransport } from 'nodemailer'
+import { logger } from "../utils/logger.js"
+import dotenv from 'dotenv'
+dotenv.config()
 /* --------- --------- */
-const MAIL = process.env.MAIL;
-const MAIL_PASSWORD = process.env.MAIL_PASSWORD;;
-const MAIL_ADMIN = process.env.MAIL_ADMIN;
-/* --------- --------- */
+const MAIL = process.env.MAIL
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD
+const MAIL_ADMIN = process.env.MAIL_ADMIN
+/* --------- No me toma en cuenta los .env que importo, además también que puse las credenciales en el node_modules --------- */
 export const sendEmailNewUser = async ( user ) =>{
     const transporter = createTransport({
-        service: 'gmail',
+        host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: MAIL,
-            pass: MAIL_PASSWORD,
+            user: 'aric16@ethereal.email' || MAIL,
+            pass: 'Xa958vf74cNVTPNKRX' || MAIL_PASSWORD,
         },
-    });
+    })
     
     const mailOptions = {
-        from: 'Servidor Backend <noreply@example.com>',
-        to: MAIL_ADMIN,
+        from: 'Servidor',
+        to: 'santillanlautaro03@gmail.com' || MAIL_ADMIN,
         subject: 'Nuevo Registro',
         text: `Se ha registrado un nuevo usuario: ${user.name} con el email: ${user.userEmail}`,
     }
     try {
-        const info = await transporter.sendMail(mailOptions);
-        logger.info('Mail nuevo usuario', info);
+        const info = await transporter.sendMail(mailOptions)
+        logger.info('Mail nuevo usario' , info)
     } catch (error) {
         logger.error('Error en mailOptions', error)
     }
 }
-/* --------- --------- */
 export const sendEmailNewOrder = async ( order, total, user ) =>{
     const transporter = createTransport({
-        service: 'gmail',
+        host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: MAIL,
-            pass: MAIL_PASSWORD,
+            user: 'aric16@ethereal.email' || MAIL,
+            pass: 'Xa958vf74cNVTPNKRX' || MAIL_PASSWORD,
+        },
+        secure: false,
+        tls: {
+            rejectUnauthorized: false
         }
     })
     
     const mailOptions = {
-        from: 'Servidor Backend <noreply@example.com>',
-        to: MAIL_ADMIN, 
+        from: 'Servidor',
+        to: 'santillanlautaro03@gmail.com' || MAIL_ADMIN, 
         subject: 'Nueva Orden',
         html: `<p>Se ha realizado un nuevo pedido por el usuario ${user.name} con el email: ${user.userEmail} y el teléfono: ${user.phone} con el siguiente detalle: ${order} , \nTOTAL: $ ${total}</p>`,
     }
     try {
-        const info = await transporter.sendMail(mailOptions);
-        logger.info('Mail nueva orden enviado' , info);
+        const info = await transporter.sendMail(mailOptions)
+        logger.info('Mail nueva orden enviado' , info)
     } catch (error) {
-        logger.error('Error en mailOptions', error);
+        logger.error('Error en mailOptions', error)
     }
-};
+}
--- src/services/twilio.js
+++ src/services/twilio.js
@@ -2,22 +2,22 @@ import { logger } from "../utils/logger.js";
 import twilio from 'twilio';
 import dotenv from 'dotenv';
 dotenv.config()
-/* --------- --------- */
+/* --------- No me toma las credenciales del .env --------- */
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 const PHONE_ADMIN = process.env.PHONE_ADMIN;
 const TWILIO_WHATSAPP_NUMBER = process.env.TWILIO_WHATSAPP_NUMBER;
 /* --------- --------- */
-const accountSid = TWILIO_ACCOUNT_SID;
-const authToken = TWILIO_AUTH_TOKEN;
+const accountSid = 'AC4aebb50dbc7a7c8fd227c88e35e93605' || TWILIO_ACCOUNT_SID;
+const authToken = '8c96461ea0082e271848ad344bde51af' || TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 /* --------- --------- */
 export const sendNewOrder = async (order, user) => {
     try {
         const option = {
-            from: TWILIO_PHONE_NUMBER,
-            to: PHONE_ADMIN,
+            from: '+15304445602' || TWILIO_PHONE_NUMBER,
+            to: 'whatsapp:+5491165799996' || PHONE_ADMIN,
             body: `Se ha realizado un nuevo pedido por el usuario ${user.name} con el email: ${user.userEmail} y el teléfono: ${user.phone} con el siguiente detalle: ${order}`,
         }
         logger.info('Mensaje de orden enviado');
@@ -30,8 +30,8 @@ export const sendNewOrder = async (order, user) => {
 export const sendWhatsApp = async (order, total, user) => {
     try {
         const option = {
-            from: TWILIO_WHATSAPP_NUMBER,
-            to: `whatsapp:${user.phone}` || `whatsapp:${PHONE_ADMIN}`,
+            from: 'whatsapp:+14155238886' || TWILIO_WHATSAPP_NUMBER,
+            to: 'whatsapp:+5491165799996' || `whatsapp:${user.phone}` || `whatsapp:${PHONE_ADMIN}`,
             body: `Se ha realizado un nuevo pedido por el usuario ${user.name} con el email: ${user.userEmail} y el teléfono: ${user.phone} con el siguiente detalle: ${order} \nTOTAL: $ ${total}`,
         };
         logger.info('Mensaje de orden enviado por WhatsApp');
@@ -44,8 +44,8 @@ export const sendWhatsApp = async (order, total, user) => {
 export const sendWhatsAppAdmin = async (order, total, user) => {
     try {
         const option = {
-            from: TWILIO_WHATSAPP_NUMBER,
-            to: `whatsapp:${PHONE_ADMIN}`,
+            from: 'whatsapp:+14155238886' || TWILIO_WHATSAPP_NUMBER,
+            to: 'whatsapp:+5491165799996' || PHONE_ADMIN,
             body: `Se ha realizado un nuevo pedido por el usuario ${user.name} con el email: ${user.userEmail} y el teléfono: ${user.phone} con el siguiente detalle: ${order} \nTOTAL: $ ${total}`,
         };
         logger.info('Mensaje de orden enviado por WhatsApp /sendWhatsAppAdmin');
