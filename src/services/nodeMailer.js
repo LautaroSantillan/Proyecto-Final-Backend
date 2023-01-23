5dfb66068103e7d565a7db76e2973d5e27c1bcea
lautarosantillan <santillanlautaro03@gmail.com>
2023-01-22 22:04:54 -0300

    New email format HTML -v21

--- src/services/nodeMailer.js
+++ src/services/nodeMailer.js
@@ -1,11 +1,11 @@
-import { createTransport } from 'nodemailer'
-import { logger } from "../utils/logger.js"
-import dotenv from 'dotenv'
+import { createTransport } from 'nodemailer';
+import { logger } from "../utils/logger.js";
+import dotenv from 'dotenv';
 dotenv.config()
 /* --------- --------- */
-const MAIL = process.env.MAIL
-const MAIL_PASSWORD = process.env.MAIL_PASSWORD
-const MAIL_ADMIN = process.env.MAIL_ADMIN
+const MAIL = process.env.MAIL;
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+const MAIL_ADMIN = process.env.MAIL_ADMIN;
 /* --------- No me toma en cuenta los .env que importo, además también que puse las credenciales en el node_modules --------- */
 export const sendEmailNewUser = async ( user ) =>{
     const transporter = createTransport({
@@ -15,21 +15,25 @@ export const sendEmailNewUser = async ( user ) =>{
             user: 'aric16@ethereal.email' || MAIL,
             pass: 'Xa958vf74cNVTPNKRX' || MAIL_PASSWORD,
         },
-    })
+    });
     
     const mailOptions = {
         from: 'Servidor',
         to: 'santillanlautaro03@gmail.com' || MAIL_ADMIN,
-        subject: 'Nuevo Registro',
-        text: `Se ha registrado un nuevo usuario: ${user.name} con el email: ${user.userEmail}`,
-    }
+        subject: 'Nuevo Registro de Usuario',
+        html: `<html><h1>Se ha registrado un nuevo usuario:</h1>
+            <hr>
+            <p><span style="font-weight: bolder">${user.name}</span>, con el email: <span style="font-weight: bolder">${user.userEmail}</span></p></html>`,
+    };
+
     try {
-        const info = await transporter.sendMail(mailOptions)
-        logger.info('Mail nuevo usario' , info)
+        const info = await transporter.sendMail(mailOptions);
+        logger.info('Mail nuevo usario' , info);
     } catch (error) {
-        logger.error('Error en mailOptions', error)
+        logger.error('Error en mailOptions', error);
     }
 }
+
 export const sendEmailNewOrder = async ( order, total, user ) =>{
     const transporter = createTransport({
         host: 'smtp.ethereal.email',
@@ -42,18 +46,25 @@ export const sendEmailNewOrder = async ( order, total, user ) =>{
         tls: {
             rejectUnauthorized: false
         }
-    })
+    });
     
     const mailOptions = {
         from: 'Servidor',
         to: 'santillanlautaro03@gmail.com' || MAIL_ADMIN, 
-        subject: 'Nueva Orden',
-        html: `<p>Se ha realizado un nuevo pedido por el usuario ${user.name} con el email: ${user.userEmail} y el teléfono: ${user.phone} con el siguiente detalle: ${order} , \nTOTAL: $ ${total}</p>`,
-    }
+        subject: 'Nueva Orden de Compra',
+        html: `<html><h1>Se ha realizado un nuevo pedido:</h1> 
+            <hr>
+            <p>Por el usuario ${user.name}, con el email: ${user.userEmail} y el teléfono: ${user.phone}</p>
+            <br>
+            <p><span style="text-decoration: underline">Con el siguiente detalle:</span> ${order}</p> 
+            <br>
+            <p style="font-weight: bolder"><span style="text-decoration: underline">TOTAL:</span> $${total}</p></html>`,
+    };
+
     try {
-        const info = await transporter.sendMail(mailOptions)
-        logger.info('Mail nueva orden enviado' , info)
+        const info = await transporter.sendMail(mailOptions);
+        logger.info('Mail nueva orden enviado' , info);
     } catch (error) {
-        logger.error('Error en mailOptions', error)
+        logger.error('Error en mailOptions', error);
     }
 }
